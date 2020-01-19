63f93034e6a2f7489e77c07a19abf8104d62e118
Szymon <Szymon@123.com>
2020-01-19 20:12:42 +0100

    added game mode choice and pawn choice

--- game.py
+++ game.py
@@ -1,9 +1,12 @@
 class Game:
 
-    def __init__(self, gamemode = None, player = None):
-        self.__gameBoard = {'UL': 'X', 'UM': '', 'UR': '', 'ML': '', 'MM': '', 'MR': '', 'LL': '', 'LM': '', 'LR': ''}
+    def __init__(self, gamemode=None, fpawn=None, spawn=None):
+        self.__gameBoard = {'UL': '', 'UM': '', 'UR': '', 'ML': '', 'MM': '', 'MR': '', 'LL': '', 'LM': '', 'LR': ''}
         self.__game_mode = gamemode
-        self.__player = player
+        self.__f_player_pawn = fpawn
+        self.__s_player_pawn = spawn
+        self.__move_counter = 0
+        print(f'Selected {self.__game_mode} mode first player pawn is {self.__f_player_pawn}')
 
     def set_game_mode(self, gamemode):
         self.__game_mode = gamemode
@@ -11,64 +14,92 @@ class Game:
     def get_game_mode(self):
         return self.__game_mode
 
-    def write_to_board(self, command):
-        #decoding command
-        place = command[0] + command[1]
-        sign = command[3]
-        self.__gameBoard[place] = sign
-
     def get_game_board(self):
         return self.__gameBoard
+
+    def get_move_counter(self):
+        return self.__move_counter
 
     def get_game_board_field(self, index):
         return self.__gameBoard[index]
 
-    def check_win_conditions(self):
-        win_flag = False
-        keyfirstletterindex = 0
-        keysecondletterindex = 1
-        #check rows
-        for k, v in self.__gameBoard.items():
-            if v == '':
-                continue
+    def __check_if_field_available(self, index):
+        check_flag = False
+        if self.__gameBoard[index] == '':
+            check_flag = True
+        return check_flag
+
+    def write_to_board(self, command):
+            #add zmieniające ruchy raz pl1 raz pl2 na podstawie __move_counter/2!
+            write_flag = False
+            # decoding command
+            place = command[0] + command[1]
+            if self.__check_if_field_available(place):
+                if not self.__move_counter % 2:
+                    self.__gameBoard[place] = self.__f_player_pawn
+                    write_flag = True
+                    self.__move_counter += 1
+                else:
+                    self.__gameBoard[place] = self.__s_player_pawn
+                    write_flag = True
+                    self.__move_counter += 1
             else:
-                key = k[keyfirstletterindex]
-                value = v
-                chflag = 0
-                for K, V in self.__gameBoard.items():
-                    if K[keyfirstletterindex] == key and V == value:
-                        chflag += 1
-                if chflag == 3:
-                    print(f"{value} You won!")
-                    win_flag = True
-                    break
-        #chcek columns
-        if not win_flag:
+                print('Field taken select other one!')
+
+            return write_flag
+
+
+
+
+    def check_win_conditions(self):
+        if self.__move_counter <= 9:# DEBUG AND REPEIR THAT NUMBER SCHOULD BE 9-number of possibe moves
+            win_flag = False
+            keyfirstletterindex = 0
+            keysecondletterindex = 1
+            #check rows
             for k, v in self.__gameBoard.items():
                 if v == '':
                     continue
                 else:
-                    key = k[keysecondletterindex]
+                    key = k[keyfirstletterindex]
                     value = v
                     chflag = 0
                     for K, V in self.__gameBoard.items():
-                        if K[keysecondletterindex] == key and V == value:
+                        if K[keyfirstletterindex] == key and V == value:
                             chflag += 1
                     if chflag == 3:
                         print(f"{value} You won!")
                         win_flag = True
                         break
-        #check diagonals
-        if not win_flag:
-            if (self.__gameBoard['UL'] == self.__gameBoard['MM'] == self.__gameBoard['LR']) and self.__gameBoard['UL'] != '' and self.__gameBoard['MM'] != '' and self.__gameBoard['LR'] != '':
-                print(f'{self.__gameBoard["UL"]} You won!')
-                win_flag = True
-            if (self.__gameBoard['UR'] == self.__gameBoard['MM'] == self.__gameBoard['LL']) and self.__gameBoard['UR'] != '' and self.__gameBoard['MM'] != '' and self.__gameBoard['LL'] != '':
-                print(f'{self.__gameBoard["UR"]} You won!')
-                win_flag = True
+            #chcek columns
+            if not win_flag:
+                for k, v in self.__gameBoard.items():
+                    if v == '':
+                        continue
+                    else:
+                        key = k[keysecondletterindex]
+                        value = v
+                        chflag = 0
+                        for K, V in self.__gameBoard.items():
+                            if K[keysecondletterindex] == key and V == value:
+                                chflag += 1
+                        if chflag == 3:
+                            print(f"{value} You won!")
+                            win_flag = True
+                            break
+            #check diagonals
+            if not win_flag:
+                if (self.__gameBoard['UL'] == self.__gameBoard['MM'] == self.__gameBoard['LR']) and self.__gameBoard['UL'] != '' and self.__gameBoard['MM'] != '' and self.__gameBoard['LR'] != '':
+                    print(f'{self.__gameBoard["UL"]} You won!')
+                    win_flag = True
+                if (self.__gameBoard['UR'] == self.__gameBoard['MM'] == self.__gameBoard['LL']) and self.__gameBoard['UR'] != '' and self.__gameBoard['MM'] != '' and self.__gameBoard['LL'] != '':
+                    print(f'{self.__gameBoard["UR"]} You won!')
+                    win_flag = True
 
-        return win_flag
-
+            return win_flag
+        else:
+            print('DRAW!')
+            return True
     def printboard(self):
         print(f'''
     {self.__gameBoard['UL']}|{self.__gameBoard['UM']}|{self.__gameBoard['UR']}
--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from game import Game
 
 if __name__ == "__main__":
-
-    # gamemode = input('Please type game mode: "2players" or "AI" and press enter')
-    # game.game_mode_set(gamemode)
-    print('Hello! here is list of commands')
-    current_game = Game('2players', 'X')
+    gamemode = input('Please type game mode: "2players" or "AI" and press enter: ')
+    pawn = input('Please select pawn "X" or "O" and press enter: ')
+    if pawn == 'X':
+        current_game = Game(gamemode, 'X', 'O')
+    elif pawn == 'O':
+        current_game = Game(gamemode, 'O', 'X')
+    print('Here is list of commands')
     Game.printlist()
 
     while True:
         command = input('Please select the field and type "X" or "O eg.(UL-O)')
-        current_game.write_to_board(command)
-        current_game.printboard()
-        if current_game.check_win_conditions():
-            break
+        if current_game.write_to_board(command):
+            current_game.printboard()
+            if current_game.check_win_conditions():
+                break
