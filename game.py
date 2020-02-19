a16c9f2ba1f5682de762acc69e71d9c18f7b3e5e
Szymon <Szymon@123.com>
2020-02-19 17:36:45 +0100

    simplyfy board checking

--- game.py
+++ game.py
@@ -1,9 +1,12 @@
 import random
 
+
 class Game:
 
     def __init__(self, gamemode=None, fpawn=None, spawn=None):
-        self.__gameBoard = {'UL': '', 'UM': '', 'UR': '', 'ML': '', 'MM': '', 'MR': '', 'LL': '', 'LM': '', 'LR': ''}
+        self.__gameBoard = {'UL': ['', (83, 62)], 'UM': ['', (282, 62)], 'UR': ['', (479, 62)],
+                            'ML': ['', (83, 237)],'MM': ['', (282, 237)], 'MR': ['', (479, 237)],
+                            'LL': ['', (83, 412)], 'LM': ['', (282, 412)], 'LR': ['', (479, 412)]}
         self.__game_mode = gamemode
         self.__f_player_pawn = fpawn
         self.__s_player_pawn = spawn
@@ -13,6 +16,9 @@ class Game:
 
     def set_game_mode(self, gamemode):
         self.__game_mode = gamemode
+
+    def set_game_board_field_pawn(self, index, pawn):
+        self.__gameBoard[index][0] = pawn
 
     def get_game_mode(self):
         return self.__game_mode
@@ -25,6 +31,13 @@ class Game:
 
     def get_game_board_field(self, index):
         return self.__gameBoard[index]
+
+    def get_game_board_field_pawn(self, index):
+        return self.__gameBoard[index][0]
+
+    def get_game_board_field_pos(self, index):
+        return self.__gameBoard[index][1]
+
     def bot_moves(self):
         while True:
             place = random.choice(list(self.__gameBoard))
@@ -34,22 +47,21 @@ class Game:
 
     def __check_if_field_available(self, index):
         check_flag = False
-        if self.__gameBoard[index] == '':
+        if self.get_game_board_field_pawn(index) == '':
             check_flag = True
         return check_flag
 
     def write_to_board(self, command):
-            #add zmieniające ruchy raz pl1 raz pl2 na podstawie __move_counter/2!
             write_flag = False
             # decoding command
             place = command[0] + command[1]
             if self.__check_if_field_available(place):
                 if not self.__move_counter % 2:
-                    self.__gameBoard[place] = self.__f_player_pawn
+                    self.set_game_board_field_pawn(place, self.__f_player_pawn)
                     write_flag = True
                     self.__move_counter += 1
                 else:
-                    self.__gameBoard[place] = self.__s_player_pawn
+                    self.set_game_board_field_pawn(place, self.__s_player_pawn)
                     write_flag = True
                     self.__move_counter += 1
             else:
@@ -62,39 +74,65 @@ class Game:
 
     def check_win_conditions(self):
             win_flag = False
-            keyfirstletterindex = 0
-            keysecondletterindex = 1
+            # keyfirstletterindex = 0
+            # keysecondletterindex = 1
             #check rows
-            for k, v in self.__gameBoard.items():
-                if v == '':
-                    continue
-                else:
-                    key = k[keyfirstletterindex]
-                    value = v
-                    chflag = 0
-                    for K, V in self.__gameBoard.items():
-                        if K[keyfirstletterindex] == key and V == value:
-                            chflag += 1
-                    if chflag == 3:
-                        print(f"{value} You won!")
-                        win_flag = True
-                        return win_flag
+            if (self.__gameBoard['UL'][0] == self.__gameBoard['UM'][0] == self.__gameBoard['UR'][0]) and self.__gameBoard['UL'][0] != '':
+                print(f"{self.__gameBoard['UL']} You won!")
+                win_flag = True
+                return win_flag
+            elif (self.__gameBoard['ML'][0] == self.__gameBoard['MM'][0] == self.__gameBoard['MR'][0]) and self.__gameBoard['ML'][0] != '':
+                print(f"{self.__gameBoard['ML']} You won!")
+                win_flag = True
+                return win_flag
+            elif (self.__gameBoard['LL'][0] == self.__gameBoard['LM'][0] == self.__gameBoard['LR'][0]) and self.__gameBoard['LL'][0] != '':
+                print(f"{self.__gameBoard['LL']} You won!")
+                win_flag = True
+                return win_flag
+
+            # for k, v in self.__gameBoard.items():
+            #     if v == '':
+            #         continue
+            #     else:
+            #         key = k[keyfirstletterindex]
+            #         value = v
+            #         chflag = 0
+            #         for K, V in self.__gameBoard.items():
+            #             if K[keyfirstletterindex] == key and V == value:
+            #                 chflag += 1
+            #         if chflag == 3:
+            #             print(f"{value} You won!")
+            #             win_flag = True
+            #             return win_flag
             #chcek columns
-            if not win_flag:
-                for k, v in self.__gameBoard.items():
-                    if v == '':
-                        continue
-                    else:
-                        key = k[keysecondletterindex]
-                        value = v
-                        chflag = 0
-                        for K, V in self.__gameBoard.items():
-                            if K[keysecondletterindex] == key and V == value:
-                                chflag += 1
-                        if chflag == 3:
-                            print(f"{value} You won!")
-                            win_flag = True
-                            return win_flag
+            elif (self.__gameBoard['UL'][0] == self.__gameBoard['ML'][0] == self.__gameBoard['LL'][0]) and self.__gameBoard['UL'][0] != '':
+                print(f"{self.__gameBoard['UL']} You won!")
+                win_flag = True
+                return win_flag
+            elif (self.__gameBoard['UM'][0] == self.__gameBoard['MM'][0] == self.__gameBoard['LM'][0]) and self.__gameBoard['UM'][0] != '':
+                print(f"{self.__gameBoard['UM']} You won!")
+                win_flag = True
+                return win_flag
+            elif (self.__gameBoard['UR'][0] == self.__gameBoard['MR'][0] == self.__gameBoard['LR'][0]) and self.__gameBoard['UR'][0] != '':
+                print(f"{self.__gameBoard['UR']} You won!")
+                win_flag = True
+                return win_flag
+
+            # if not win_flag:
+            #     for k, v in self.__gameBoard.items():
+            #         if v == '':
+            #             continue
+            #         else:
+            #             key = k[keysecondletterindex]
+            #             value = v
+            #             chflag = 0
+            #             for K, V in self.__gameBoard.items():
+            #                 if K[keysecondletterindex] == key and V == value:
+            #                     chflag += 1
+            #             if chflag == 3:
+            #                 print(f"{value} You won!")
+            #                 win_flag = True
+            #                 return win_flag
             #check diagonals
             if not win_flag:
                 if (self.__gameBoard['UL'] == self.__gameBoard['MM'] == self.__gameBoard['LR']) and self.__gameBoard['UL'] != '' and self.__gameBoard['MM'] != '' and self.__gameBoard['LR'] != '':
@@ -113,9 +151,12 @@ class Game:
 
     def printboard(self):
         print(f'''
-    {self.__gameBoard['UL']}|{self.__gameBoard['UM']}|{self.__gameBoard['UR']}
-    {self.__gameBoard['ML']}|{self.__gameBoard['MM']}|{self.__gameBoard['MR']}
-    {self.__gameBoard['LL']}|{self.__gameBoard['LM']}|{self.__gameBoard['LR']}''')
+    {self.__gameBoard['UL'][0]}|{self.__gameBoard['UM'][0]}|{self.__gameBoard['UR'][0]}
+    {self.__gameBoard['ML'][0]}|{self.__gameBoard['MM'][0]}|{self.__gameBoard['MR'][0]}
+    {self.__gameBoard['LL'][0]}|{self.__gameBoard['LM'][0]}|{self.__gameBoard['LR'][0]}''')
+
+
+
 
     @staticmethod
     def printlist():
@@ -129,3 +170,5 @@ class Game:
                 LL - Lower Left Corner
                 LM - Lower Middle Field
                 LR - Lower Right Corner''')
+
+
